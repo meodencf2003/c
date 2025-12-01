8a200183c7d6e8992953ad4f07cd19f44a350cb7
meodencf2003 <meodencf@gmail.com>
2025-12-01 17:50:45 +0700

    Add files via upload

---
+++ buoi3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+int main(){
+	
+	//khoi tao bien(tao bien ma khong gan gia tri)
+	 
+	//khai bao bien(tao bien co gan gia tri)
+	//sytax: kieu_du_lieu_ten_bien;
+//	int a;
+//	float b;
+//	double c;
+//	char d;
+//	
+//	int a = 2;
+//	float b = 8.9;
+//	double c = 9.999999;
+//	char d = 'A';
+//	
+	//prinft ham in
+	//scanf ham nhap
+	
+	//yeu cau nguoi dung nhap a,b sau do in ra man ket qua a-b
+//	int a,b;
+//	
+//	printf("nhap a:");
+//	scanf("%d",&a);
+//	printf("nhap b:");
+//	scanf("%d",&b);
+//	
+//	printf("a-b= %d\n",a-b);
+//	printf("a*b= %d\n",a*b);
+//	printf("a/b= %.2f\n",(float)a/b);//ep kieu so thuc
+//	printf("dia chi cua a trong ram la: %p\n", &a);//%p la toan tu  dia chi cua a ,dau & la toan tu dia chi
+//	printf("%d chia  %d du %d\n", a, b,a%b);
+//	printf("gia tri cua a la %d\n", a);
+//	printf("gia tri cua a la %d\n", ++a);//++a thuc hien bieu thuc sau khi cong 1 don vi
+//	printf("gia tri cua a la %d\n", a++);//a++ thuc hien cong 1 don vi sau khi thuc hien bieu thuc
+//	//neu a++,++a dat rieng thi no deu cong 1 don vi
+//	//a--,--a cung vay ma la giam 1 don vi
+//	//trong c 0 la dai dien cho false (sai) so khac khong la true(dung) thuong la so 1
+//	printf("a +=5 la: %d\n",a +=5);
+//	printf("a -=5 la: %d\n",a -=5);
+//	printf("a *=5 la: %d\n",a *=5);
+//	printf("a /=5 la: %d\n",a =5);
+	int x = 5,y = 3;
+	printf("x>y: %d\n",x >  y);
+	printf("x<y: %d\n",x <  y);
+	printf("x>y: %d\n",x == y);
+	printf("x=y: %d\n",x == y);
+	printf("x<=y: %d\n",x <= y);
+	printf("x>=y: %d\n",x >= y);
+	printf("x!=y: %d\n",x != y);
+//	a=a+1;
+//	printf("gia tri cua a la %d\n", a);
+
+	return 0;
+}
